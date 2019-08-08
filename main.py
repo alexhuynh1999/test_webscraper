4cf51d288961657ebd4767913f5234d42852f0f9
Alex <alexhuynh1999@gmail.com>
2019-08-08 01:58:09 -0500

    reads off the name of the entry in the data table.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def save_html(html, path):
@@ -10,15 +11,13 @@ def open_html(path):
     with open(path, 'rb') as f:
         f.read()
 
+
 url = 'https://www.allsides.com/media-bias/media-bias-ratings'
 
-from bs4 import BeautifulSoup
-
 r = requests.get(url)
-print(r.content[:100])
 soup = BeautifulSoup(r.content, 'html.parser')
 rows = soup.select('tbody tr')
-row = rows[0]
+row = rows[1]
 name = row.select_one('.source-title').text.strip()
 print(name)
 
