bea2c67f1a57763528e16510f8c510d4d262ce47
Alex <alexhuynh1999@gmail.com>
2019-08-10 17:13:20 -0500

    slightly changed approach, took forever to realize i had to look at the actual HTML not the direct CSS

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
 
+url = 'https://www.indeed.com/jobs?q=software%20developer&l=Seattle,%20WA&advn=6613095622180756'
 
-def save_html(html, path):
-    with open(path, 'wb') as f:
-        f.write(html)
+client = uReq(url)
+page_html = client.read()
+client.close()
 
-
-def open_html(path):
-    with open(path, 'rb') as f:
-        f.read()
-
-
-url = 'https://www.indeed.com/jobs?q=software%20developer&l=Seattle%2C%20WA&advn=6613095622180756'
-
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-rows = soup.select_one('.pageContent')
-print(rows)
+page_soup = soup(page_html, "html.parser")
+results = page_soup.findAll("td", {"id":"resultsCol"})
+containers = page_soup.findAll("div",{"class":"jobsearch-SerpJobCard unifiedRow row result"})
