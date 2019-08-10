1035245bb9934a07245fb32c7794e74ef6e55337
Alex <alexhuynh1999@gmail.com>
2019-08-10 17:54:56 -0500

    weird error where the company doesn't always show - perhaps the company isnt consistently located in the same spot

--- main.py
+++ main.py
@@ -10,7 +10,23 @@ client.close()
 page_soup = soup(page_html, "html.parser")
 containers = page_soup.findAll("div", {"class":"jobsearch-SerpJobCard unifiedRow row result"})
 
+total = len(containers)
+counter = 0;
 for container in containers:
-    company = container.find("a",{'data-tn-element':"companyName"}).text.strip()
+    try:
+        company_container = container.findAll("span",{'class':'company'})
+        company = company_container[0].a.text.strip()
+        counter += 1
+    except:
+        print("")
+
     title = container.div.a["title"]
-    summary = container.find("div",{'class':'summary'}).text.strip()
+
+    summary_container = container.find("div",{'class':'summary'})
+    summary = summary_container.text.strip()
+
+    print("company: " + company)
+    print("title: " + title)
+    print("summary: " + summary + "\n")
+
+print(str(counter) + "/" + str(total) + " results shown")
